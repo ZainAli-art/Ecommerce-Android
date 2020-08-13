4d0a593f8919e8a7063e1ced227f4b1c0be94f9a
ZainAli7777 <z.a.cheema7777@gmail.com>
2020-08-13 19:00:03 +0500

    Added ViewModel and moved Login logic to it

--- app/src/main/java/com/example/android/ecommerce/SignInFragment.java
+++ app/src/main/java/com/example/android/ecommerce/SignInFragment.java
@@ -5,10 +5,10 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -16,26 +16,13 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-import com.android.volley.AuthFailureError;
-import com.android.volley.Request;
-import com.android.volley.toolbox.JsonObjectRequest;
-import com.android.volley.toolbox.StringRequest;
-
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.util.HashMap;
-import java.util.Map;
-
-import static com.example.android.ecommerce.MySingleton.HOST_URL;
+import com.example.android.ecommerce.viewmodel.CustomerViewModel;
 
 public class SignInFragment extends Fragment implements View.OnClickListener {
-    private static final String TAG = "SignInFragment";
-    public static final String INSERT_CUSTOMER_URL = HOST_URL + "scripts/insert-customer.php";
-
     private EditText email;
     private EditText password;
     private Button loginBtn;
+    private CustomerViewModel viewModel;
 
     private NavController navController;
 
@@ -53,49 +40,32 @@ public class SignInFragment extends Fragment implements View.OnClickListener {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        // initialize components
+        navController = Navigation.findNavController(view);
         email = view.findViewById(R.id.email);
         password = view.findViewById(R.id.password);
         loginBtn = view.findViewById(R.id.loginBtn);
         loginBtn.setOnClickListener(this);
+        viewModel = new ViewModelProvider(
+                requireActivity(),
+                new ViewModelProvider.AndroidViewModelFactory(requireActivity().getApplication()))
+                .get(CustomerViewModel.class);
 
-        navController = Navigation.findNavController(view);
-    }
-
-    public void login(String email, String password) {
-        StringRequest request = new StringRequest(
-                Request.Method.POST,
-                INSERT_CUSTOMER_URL,
-                response -> {
-                    try {
-                        JSONObject jsonObject = new JSONObject(response);
-                        int uid = jsonObject.getInt("uid");
-                        moveToHomeFragment(uid);
-                    } catch (JSONException e) {
-                        e.printStackTrace();
-                    }
-                },
-                error -> {
-                    Toast.makeText(getContext(), "URL response error", Toast.LENGTH_SHORT).show();
-                }
-        ) {
-            @Override
-            protected Map<String, String> getParams() throws AuthFailureError {
-                Log.d(TAG, "getParams: called");
-                Map<String, String> params = new HashMap<>();
-                params.put("email", email);
-                params.put("pwd", password);
-                return params;
+        // add observers to live data from view model
+        viewModel.getCustomer().observe(getViewLifecycleOwner(), customer -> {
+            if (customer == null) {
+                Toast.makeText(getContext(), "Invalid Credentials", Toast.LENGTH_SHORT).show();
+            } else {
+                moveToHomeFragment(customer.getId());
             }
-        };
-
-        MySingleton.getInstance(getContext()).enqueueRequest(request);
+        });
     }
 
     @Override
     public void onClick(View view) {
         switch (view.getId()) {
             case R.id.loginBtn:
-                login(email.getText().toString(), password.getText().toString());
+                viewModel.login(email.getText().toString(), password.getText().toString());
                 break;
             default:
                 Toast.makeText(getContext(), "No functionality assigned yet.", Toast.LENGTH_SHORT).show();
@@ -103,12 +73,8 @@ public class SignInFragment extends Fragment implements View.OnClickListener {
     }
 
     public void moveToHomeFragment(int uid) {
-        if (uid == -1) {
-            Toast.makeText(getContext(), "Invalid Credentials", Toast.LENGTH_SHORT).show();
-        } else {
-            Bundle args = new Bundle();
-            args.putInt("uid", uid);
-            navController.navigate(R.id.action_signInFragment_to_homeFragment, args);
-        }
+        Bundle args = new Bundle();
+        args.putInt("uid", uid);
+        navController.navigate(R.id.action_signInFragment_to_homeFragment, args);
     }
 }
---
+++ app/src/main/java/com/example/android/ecommerce/viewmodel/CustomerViewModel.java
@@ -0,0 +1,72 @@
+package com.example.android.ecommerce.viewmodel;
+
+import android.app.Application;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+
+import com.android.volley.Request;
+import com.android.volley.toolbox.StringRequest;
+import com.example.android.ecommerce.MySingleton;
+import com.example.android.ecommerce.model.Customer;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static com.example.android.ecommerce.MySingleton.HOST_URL;
+
+public class CustomerViewModel extends AndroidViewModel {
+    public static final String INSERT_CUSTOMER_URL = HOST_URL + "scripts/insert-customer.php";
+
+    private MutableLiveData<Customer> mCustomer;
+
+    public CustomerViewModel(@NonNull Application application) {
+        super(application);
+        mCustomer = new MutableLiveData<>();
+    }
+
+    public LiveData<Customer> getCustomer() {
+        return mCustomer;
+    }
+
+    public void setCustomer(Customer customer) {
+        mCustomer.setValue(customer);
+    }
+
+    public void login(String email, String password) {
+        StringRequest request = new StringRequest(
+                Request.Method.POST,
+                INSERT_CUSTOMER_URL,
+                response -> {
+                    try {
+                        JSONObject jsonObject = new JSONObject(response);
+                        int uid = jsonObject.getInt("uid");
+                        if (uid == -1) {
+                            setCustomer(null);
+                        } else {
+                            setCustomer(new Customer(uid, email, password));
+                        }
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                },
+                error -> Toast.makeText(getApplication().getApplicationContext(), "URL response error", Toast.LENGTH_SHORT).show()
+        ) {
+            @Override
+            protected Map<String, String> getParams() {
+                Map<String, String> params = new HashMap<>();
+                params.put("email", email);
+                params.put("pwd", password);
+                return params;
+            }
+        };
+
+        MySingleton.getInstance(getApplication().getApplicationContext()).enqueueRequest(request);
+    }
+}
