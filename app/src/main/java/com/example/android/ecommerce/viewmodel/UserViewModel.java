0b93712fae7c95f40ffd20dbd4fd067ea0b4eb74
Zain Ali <z.a.cheema7777@gmail.com>
2020-08-15 21:02:00 +0500

    Synched Google login info synched to LAMP server on first Login

--- app/src/main/java/com/example/android/ecommerce/viewmodel/UserViewModel.java
+++ app/src/main/java/com/example/android/ecommerce/viewmodel/UserViewModel.java
@@ -3,12 +3,17 @@ package com.example.android.ecommerce.viewmodel;
 import android.app.Application;
 import android.content.Context;
 import android.net.Uri;
+import android.util.Log;
 
 import androidx.annotation.NonNull;
 import androidx.lifecycle.AndroidViewModel;
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.toolbox.StringRequest;
+import com.example.android.ecommerce.MySingleton;
 import com.example.android.ecommerce.model.User;
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
@@ -17,7 +22,12 @@ import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.tasks.Task;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class UserViewModel extends AndroidViewModel {
+    private static final String TAG = "UserViewModel";
+    private static final String ADD_USER_URL = MySingleton.HOST_URL + "scripts/add-user.php";
     private Context mContext;
     private MutableLiveData<User> mUser;
     private GoogleSignInOptions gso;
@@ -65,6 +75,7 @@ public class UserViewModel extends AndroidViewModel {
     }
 
     public void setUser(User user) {
+        addUserToServer(user);
         mUser.setValue(user);
     }
 
@@ -75,5 +86,33 @@ public class UserViewModel extends AndroidViewModel {
         } catch (ApiException e) {
             setUser(null);
         }
+    }
+    
+    public void addUserToServer(User user) {
+        if (user == null) return;
+
+        Log.d(TAG, "addUserToServer: processing...");
+        StringRequest request = new StringRequest(
+                Request.Method.POST,
+                ADD_USER_URL,
+                response -> {
+                    Log.d(TAG, "addUserToServer response: " + response);
+                },
+                error -> {}
+        ) {
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Log.d(TAG, "getParams: called");
+                Map<String, String> params = new HashMap<>();
+                params.put("uid", user.getUid());
+                params.put("email", user.getEmail());
+                params.put("fullname", user.getFullName());
+                String imgUrl = user.getImgUrl() == null ? "null" : user.getImgUrl().toString();
+                params.put("img_dir", imgUrl);
+                return params;
+            }
+        };
+
+        MySingleton.getInstance(mContext).enqueueRequest(request);
     }
 }
